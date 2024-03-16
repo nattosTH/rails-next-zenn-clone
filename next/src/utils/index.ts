e87570ac4fec285df8806701794a0758e3a16cbc
natto <natto@nattos-MacBook-Air.local>
2024-03-16 16:14:54 +0700

    prepared test enviroment

---
+++ next/src/utils/index.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosResponse, AxiosError } from 'axios'
+
+export const fetcher = (url: string) => {
+  return axios
+    .get(url)
+    .then((res: AxiosResponse) => res.data)
+    .catch((err: AxiosError) => {
+      console.log(err.message)
+      throw err
+    })
+}
